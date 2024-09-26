a27d9cd860d36c2a9ff100b490bdf2d19a8ac8d3
KingSplatt <malp18774@gmail.com>
2024-09-25 17:59:04 -0700

    Estilo de entrada de la webapp

--- techtionary/src/Log.js
+++ techtionary/src/Log.js
@@ -3,26 +3,28 @@ import React from "react";
 
 const Log = () => {
     return (
-        <body>
-            <header>
-                <h1>
-                    Techtionary
-                </h1>
-            </header>
-            <section id="secc01">
-                <article>
-                    <input type="button" value="Camara"></input>
-                </article>
-                <article>
-                    <input type="button" value="Dispositivos"></input>
-                </article>
-            </section>
-            <section id="secc02">
-                <article>
-                    la camada va aqui
-                </article>
-            </section>
-        </body>
+        <html>
+            <body>
+                <header>
+                    <h1>
+                        Techtionary
+                    </h1>
+                </header>
+                <section id="secc01">
+                    <article>
+                        <input type="button" value="Camara"></input>
+                    </article>
+                    <article>
+                        <input type="button" value="Dispositivos"></input>
+                    </article>
+                </section>
+                <section id="secc02">
+                    <article>
+                        la camada va aqui
+                    </article>
+                </section>
+            </body>
+        </html>
     );
 };
 
