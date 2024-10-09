28534a6fb1cd6e19d3ba367f081507abc1304935
KingSplatt <malp18774@gmail.com>
2024-10-09 11:56:47 -0700

    estilo

--- techtionary/src/Log.js
+++ techtionary/src/Log.js
@@ -11,18 +11,20 @@ const Log = () => {
                         Techtionary
                     </h1>
                 </header>
-                <section id="secc01">
-                    <article>
-                        <input type="button" value="Camara"></input>
-                    </article>
-                    <article>
-                        <input type="button" value="Dispositivos"></input>
-                    </article>
-                </section>
-                <section id="secc02">
-                    <article>
-                        <Camara />
-                    </article>
+                <section id="contenedor">
+                    <section id="secc01">
+                        <article>
+                            <input type="button" value="Camara"></input>
+                        </article>
+                        <article>
+                            <input type="button" value="Dispositivos"></input>
+                        </article>
+                    </section>
+                    <section id="secc02">
+                        <article>
+                            <Camara />
+                        </article>
+                    </section>
                 </section>
             </body>
         </html>
